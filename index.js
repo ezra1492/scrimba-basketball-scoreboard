85222dcff3de06a326a7d9bd91998acd28a3ebe4
ezra1492 <israelguevara1492@gmail.com>
2026-06-24 09:53:20 -0500

    debug: verifying event logic connectivity via console output

---
+++ index.js
@@ -0,0 +1,27 @@
+// HOME BUTTONS ↓
+function addOneHome() {
+  console.log("+1 home");
+}
+
+function addTwoHome() {
+  console.log("+2 home");
+}
+
+function addThreeHome() {
+  console.log("+3 home");
+}
+// HOME BUTTONS ↑
+
+// GUEST BUTTONS ↓
+function addOneGuest() {
+  console.log("+1 Guest");
+}
+
+function addTwoGuest() {
+  console.log("+2 Guest");
+}
+
+function addThreeGuest() {
+  console.log("+3 Guest");
+}
+// GUEST BUTTONS ↑
